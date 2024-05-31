7572de27022215cd5c1974bc120a1da5c2bec65d
Leonardo Palomino <l.palomino1@icloud.com>
2024-05-31 13:40:56 +0200

    get leaderboard entries also for single community

--- src/leaderboards/index.ts
+++ src/leaderboards/index.ts
@@ -1,6 +1,6 @@
 import { db } from "@db";
-import { communityMembers, teams, users } from "@schema";
-import { and, asc, desc, eq, ilike, inArray, or } from "drizzle-orm";
+import { communities, communities, communityMembers, teams, tournaments, users } from "@schema";
+import { and, asc, count, desc, eq, ilike, inArray, or } from "drizzle-orm";
 import { Hono } from "hono";
 import { jwt } from "hono/jwt";
 
@@ -68,7 +68,7 @@ leaderboardsRoute.get('/', async (c) => {
     });
 
     if (!communityExists) {
-        return c.json({ message: "Unauthorized access to leaderboard preview." }, 401);
+        return c.json({ message: "Community not found." }, 404);
     }
 
     const result = await db.select({
@@ -97,49 +97,106 @@ leaderboardsRoute.get('/', async (c) => {
     })));
 });
 
-leaderboardsRoute.get('/previews', async (c) => {
-    const { sub } = c.get('jwtPayload');
-
-    const attendingCommunities = await db.query.communityMembers.findMany({
-        where: eq(communityMembers.username, sub),
+const getLeaderboardDataForCommunity = async (communityId: string, sub: string) => {
+    const communityData = await db.query.communityMembers.findFirst({
+        where: and(eq(communityMembers.username, sub), eq(communityMembers.communityId, communityId)),
         with: {
+            user: {
+                with: {
+                    supports: true,
+                },
+                columns: {
+                    username: true,
+                    name: true,
+                    joinedAt: true,
+                    points: true
+                }
+            },
             community: {
                 with: {
-                    tournament: true,
-                    members: {
-                        with: {
-                            user: {
-                                with: {
-                                    supports: true
-                                },
-                                columns: {
-                                    username: true,
-                                    name: true,
-                                    points: true,
-                                    joinedAt: true
-                                }
-                            }
-                        },
-                        columns: { }
-                    }
+                    tournament: true
+                },
+                columns: {
+                    id: true,
+                    name: true
                 }
             }
         },
-        columns: {
-            position: true
-        }
+        columns: { position: true }
     });
 
-    if (!attendingCommunities.length) {
-        return c.json([]);
+    if (!communityData) {
+        throw new Error('The community was not found.')
     }
 
-    const result = attendingCommunities.map(({ community }) => ({
-        community: { ...community, members: undefined, tournamentId: undefined },
-        entries: getLeaderboardPreviewMembers(community.members.map(({user}) => user).sort((a, b) => b.points != a.points ? b.points - a.points : new Date(a.joinedAt).getTime() - new Date(b.joinedAt).getTime()), sub)
-    }));
+    const leaderboardData = await db.select({
+        user: users,
+        supports: teams,
+        position: communityMembers.position
+    })
+    .from(users)
+    .innerJoin(communityMembers, and(
+        eq(users.username, communityMembers.username),
+        eq(communityMembers.communityId, communityId)
+    ))
+    .leftJoin(teams, eq(teams.id, users.supportsTeamId))
+    .orderBy(desc(users.points), asc(users.joinedAt))
+
+    return {
+        community: communityData.community,
+        entries: getLeaderboardPreviewMembers(leaderboardData.map(({ user, position, supports }) => {
+            const { firebaseId, supportsTeamId, ...rest } = user;
+            return {
+                user: {
+                    ...rest,
+                    supports
+                } as User,
+                position
+            }
+        }), { user: communityData.user, position: communityData.position })
+    }
+}
+
+leaderboardsRoute.get('/previews/:communityId', async (c) => {
+    const { sub } = c.get('jwtPayload');
+    const communityId = c.req.param('communityId')
+
+    if (!communityId) {
+        return c.json({ message: "CommunityId must be provided." }, 400);
+    }
+
+    return c.json(await getLeaderboardDataForCommunity(communityId, sub));
+});
+
+leaderboardsRoute.get('/previews', async (c) => {
+    const { sub } = c.get('jwtPayload');
+
+    const joinedCommunities = await db.select({
+        id: communities.id
+    })
+    .from(communityMembers)
+    .where(eq(communityMembers.username, sub))
+    .innerJoin(communities, eq(communityMembers.communityId, communities.id))
+    
+    let result: { community: Community, entries: { user: User, position: number }[] }[] = []
+
+    for (const communityId of joinedCommunities.map(({id}) => id)) {
+        result.push(await getLeaderboardDataForCommunity(communityId, sub));
+    }
+
     return c.json(result);
   });
+
+  type Community = {
+    name: string;
+    id: string;
+    tournament: {
+        name: string;
+        id: string;
+        from: string;
+        to: string;
+    };
+}
 
   type User = {
     name: string;
@@ -155,41 +212,35 @@ leaderboardsRoute.get('/previews', async (c) => {
   /**
    * O(n) due to finding the currentUserIndex -> still faster then db.query(currentUser) because how many members are there gonna be? even with 2 mio members, its fast ah.
    */
-  const getLeaderboardPreviewMembers = (members: User[], currentUsername: string) => {
+  const getLeaderboardPreviewMembers = (members: { user: User, position: number }[], currentUser: { user: User, position: number }) => {
     let result: { user: User, position: number }[] = [];
-    const currentUserIndex = members.findIndex(({username}) => username == currentUsername);
-  
-    if (currentUserIndex == -1) {
-      throw new Error('Unauthorized access to leaderboard preview.');
-    }
+    const currentUserIndex = currentUser.position - 1;
   
     const topThree = Math.min(3, currentUserIndex + 1);
   
-    result = [...members.slice(0, topThree).map((user, index) => ({ user, position: index + 1 }))];
+    result = members.slice(0, topThree);
   
     if (members.length < 4) {
-      return result;
+        return result;
     }
   
     if (currentUserIndex > 2) {
-      result.push({ user: members[currentUserIndex], position: currentUserIndex + 1 });
-    }
-    
-    const beforeCurrentUser = currentUserIndex - 1;
-    if (currentUserIndex > 3) {
-      result.push({ user: members[beforeCurrentUser], position: beforeCurrentUser + 1 });
+        if (currentUserIndex > 3) {
+            result.push(members[currentUserIndex - 1]);
+        }
+        result.push(currentUser);
     }
   
     const afterCurrentUser = currentUserIndex + 1;
     if (members.length > afterCurrentUser) {
-      result.push({ user: members[afterCurrentUser], position: afterCurrentUser + 1 });
+      result.push(members[afterCurrentUser]);
     }
   
     const lastPlace = members.length - 1
     if (lastPlace > afterCurrentUser) {
-      result.push({ user: members[lastPlace], position: lastPlace + 1 });
+      result.push(members[lastPlace]);
     }
-    return result.sort((a, b) => a.position - b.position);
+    return result;
   }
 
   export default leaderboardsRoute;
